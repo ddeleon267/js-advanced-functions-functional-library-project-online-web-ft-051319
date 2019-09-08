5d87acbf8773ae2d5cc541080f18d73889f5864b
github username <github email address>
2019-09-08 09:11:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fi = (function() {
+const fi = (function() { //
   return {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
